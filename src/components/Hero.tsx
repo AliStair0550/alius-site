48b3fe74b4de718ccaa58dcffba8a3bf4c4a16d6
Ali Stair <ali.stair.alfarhan@gmail.com>
2026-04-08 21:02:30 +0200

    hero kasse rykket lidt ned

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -61,7 +61,7 @@ function BouncingCircle() {
   }, []);
 
   return (
-    <div className="absolute right-[4%] top-1/2 -translate-y-1/2 w-[200px] h-[200px] md:w-[400px] md:h-[400px] z-0 border border-fog">
+    <div className="absolute right-[4%] top-[55%] -translate-y-1/2 w-[200px] h-[200px] md:w-[400px] md:h-[400px] z-0 border border-fog">
       <div
         ref={circleRef}
         className="rounded-full will-change-transform"
